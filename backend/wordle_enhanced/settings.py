f05e9464c4caebdfc4d18d2c4ea2c1df65937285
Brandon Lee <brandon@planet.net>
2025-12-24 10:54:55 -0500

    Added token auth option for API

--- backend/wordle_enhanced/settings.py
+++ backend/wordle_enhanced/settings.py
@@ -157,3 +157,10 @@ STATIC_URL = "static/"
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# REST
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
